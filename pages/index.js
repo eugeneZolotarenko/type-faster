994046d2560e62c92b8e3f8b51a48c02cdc3abc0
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-12-27 13:03:02 +0100

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+
+import MainContainer from "../styles/MainContainer"
+import { Box, Card, Flex } from "rebass"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Type faster!</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <MainContainer>
+        <Box width={"80%"} maxWidth='600px'>
+          <Card
+            sx={{
+              p: 1,
+              borderRadius: 2,
+              boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
+              backgroundColor: "#f3f5f7",
+              msxWidth: "500px",
+            }}>
+            <Flex flexWrap='wrap' space={5}>
+              <Box ml='5px' px={2} width={"fit-content"} fontSize='18px' css={{ borderRadius: "5px" }} color='black' bg='transparent'>
+                Flex
+              </Box>
+              <Box ml='5px' px={2} width={"fit-content"} fontSize='18px' css={{ borderRadius: "5px" }} color='white' bg='black'>
+                FlexFlex
+              </Box>
+            </Flex>
+          </Card>
+        </Box>
+      </MainContainer>
+    </>
   )
 }
---
+++ styles/MainContainer.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const MainContainer = styled.main`
+  display: flex;
+  padding-top: 100px;
+  /* align-items: center; */
+  justify-content: center;
+  min-height: 100vh;
+  width: 100%;
+`
+
+export default MainContainer
